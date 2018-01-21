8e75bcef8b79d7b868897a7f473217e4979fb861
igor <igor.dev.py@gmail.com>
2018-01-21 17:20:42 -0300

    feat: adicionado dummy report

--- controle_ponto/routes.py
+++ controle_ponto/routes.py
@@ -1,14 +1,16 @@
 """Rotas da API."""
 
-from core.views import UserViewSet
-from ponto.views import CargaHoráriaViewSet, DiaTrabalhoViewSet, MêsTrabalhoViewSet, PontoViewSet
 from rest_framework import routers
 
+from core import views as core_views
+from ponto import views as ponto_views
+
 router = routers.DefaultRouter()
-router.register('users', UserViewSet)
-router.register('pontos', PontoViewSet)
-router.register('carga-horária', CargaHoráriaViewSet)
-router.register('meses', MêsTrabalhoViewSet)
-router.register('dias', DiaTrabalhoViewSet)
+router.register('users', core_views.UserViewSet)
+
+router.register('pontos', ponto_views.PontoViewSet)
+router.register('carga-horária', ponto_views.CargaHoráriaViewSet)
+router.register('meses', ponto_views.MêsTrabalhoViewSet)
+router.register('dias', ponto_views.DiaTrabalhoViewSet)
 
 urls = router.urls
--- functional_tests/histories.py
+++ functional_tests/histories.py
@@ -5,6 +5,7 @@ Testes funcionais estão separados dos testes de aplicação já que estão rela
 a uma aplicação específica.
 """
 
+from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.test import LiveServerTestCase
 from selenium.webdriver import Firefox
@@ -12,6 +13,12 @@ from selenium.webdriver import Firefox
 
 class FunctionalTests(LiveServerTestCase):
     """Base para os testes funcionais."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Class fixtures."""
+        super(FunctionalTests, cls).setUpClass()
+        get_user_model().objects.create_user('user', 'user@domain.com', 'pass')
 
     def setUp(self):
         """Inicializa serviços necessários para execução dos testes funcionais."""
@@ -34,4 +41,17 @@ class LoginTest(FunctionalTests):
     def test_título_login(self):
         """Verifica o título da página de login."""
         self.driver.get(self.get_live_url('dashboard'))
+
         self.assertEqual(self.driver.title, 'Login')
+
+        self.driver.find_element_by_id('login').send_keys('user')
+
+        self.driver.find_element_by_id('password').send_keys('pass')
+
+        self.driver.find_element_by_id('loginButton').click()
+
+        self.assertEqual(self.driver.title, 'Bem vindo')
+        self.driver.find_element_by_id('userDropdown').click()
+        self.driver.find_element_by_id('logoutLink').click()
+
+        self.driver.switch_to.alert.accept()
